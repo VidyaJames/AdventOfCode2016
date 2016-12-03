a252937925ecfe675c71f26fe21e659a44345a73
James Bannerman <james.bannerman@indexexchange.com>
2016-12-03 13:18:01 -0500

    working solution for day 3

---
+++ day3-1.py
@@ -0,0 +1,11 @@
+f = open('inputDay3.txt','r')
+counter = 0
+for i in f.readlines():
+	sides = [int(s) for s in i.split() if s.isdigit()]
+	sides[0] = int(sides[0])
+	sides[1] = int(sides[1])
+	sides[2] = int(sides[2])
+	sides = sorted(sides)
+	if sides[2] < sides[0] + sides[1]:
+		counter = counter + 1
+print "There are " + str(counter) + " valid triangles."
