db0f9e55cdca77b067449689bb55759462f8b5b0
James Bannerman <james.bannerman@indexexchange.com>
2016-12-05 00:53:49 -0500

    removing useless while loop

--- day5/day5-1.py
+++ day5/day5-1.py
@@ -3,12 +3,9 @@ counter = 0
 base = "ffykfhsq"
 code = ""
 while len(code) < 8:
-	done = 0
-	while done == 0:
-		text = base + str(counter)
-		hashed = hashlib.md5(text).hexdigest()
-		if hashed[0:5] == "00000":
-			code = code + hashed[5]
-			done = 1
-		counter = counter + 1
+	text = base + str(counter)
+	hashed = hashlib.md5(text).hexdigest()
+	if hashed[0:5] == "00000":
+		code = code + hashed[5]
+	counter = counter + 1
 print "the code is " + code + "."
