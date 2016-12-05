211b6b7defd4338cf11f344bea55c6830d6d54f3
James Bannerman <james.bannerman@indexexchange.com>
2016-12-05 01:09:15 -0500

    small cleanup

--- day5/day5-2.py
+++ day5/day5-2.py
@@ -6,9 +6,7 @@ while '_' in code:
 	text = base + str(counter)
 	hashed = hashlib.md5(text).hexdigest()
 	if hashed[0:5] == "00000":
-		if hashed[5].isdigit():
-			place = int(hashed[5])
-			if place >= 0 and place <= 7 and code[place] == '_':
-				code[place] = hashed[6]
+		if hashed[5] in '01234567' and code[int(hashed[5])] == '_':
+			code[int(hashed[5])] = hashed[6]
 	counter = counter + 1
 print "the code is " + "".join(code) + "."
