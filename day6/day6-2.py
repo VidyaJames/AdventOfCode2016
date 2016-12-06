f9acbc670ad5d41c140751b1cbf02f093c282d96
James Bannerman <james.bannerman@indexexchange.com>
2016-12-06 01:05:28 -0500

    I swear I know how to code

---
+++ day6/day6-2.py
@@ -0,0 +1,23 @@
+import operator
+f = open("day6-input.txt", 'r')
+
+i = 0
+dictList = []
+while i < 8:
+	dictList.append({})
+	i += 1
+
+for i in f.readlines():
+	j = 0
+	while j < len(i) - 1:
+		if i[j] not in dictList[j]:
+			dictList[j][i[j]] = 1
+		else:
+			dictList[j][i[j]] += 1
+		j += 1
+string = ""
+for i in dictList:
+	string += min(i.iteritems(), key=lambda x: x[1])[0]
+
+print "the password is " + string + "."
+
